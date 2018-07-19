efbb70bd0d1976080e5a4fce431fa4d130bfebc4
vietnv14 <39946235+vietnv14@users.noreply.github.com>
2018-07-19 17:21:10 +0700

    update NVIC

--- nvic_1/inc/hw_stm32f051r8.h
+++ nvic_1/inc/hw_stm32f051r8.h
@@ -1,3 +1,4 @@
+
 #ifndef __HW_STM32F051R8_H__
 #define __HW_STM32F051R8_H__
 
@@ -5,57 +6,86 @@
 #define write_reg(addr, value)    *((unsigned long int *)(addr)) = value
 #define read_reg(addr, mask)      *((unsigned long int *)(addr)) & (mask)
 
+#define write_reg_8(addr, value)    *((char *)(addr)) = value
+#define read_reg_8(addr, mask)      *((char *)(addr)) & (mask)
+
 /* RCC - clock */
 #define BASE_ADDR_RCC		0x40021000u
-#define RCC_APB2ENR			(BASE_ADDR_RCC + 0x18u)
+#define RCC_APB2ENR			*((unsigned int *)(BASE_ADDR_RCC + 0x18u))
+	/*ENABEL CLOCK*/
+#define RCC_APB2ENR_ENABEL_CLOCK_PORTA	(RCC_APB2ENR |= 1<<2)
+#define RCC_APB2ENR_ENABEL_CLOCK_PORTB	(RCC_APB2ENR |= 1<<3)
+#define RCC_APB2ENR_ENABEL_CLOCK_PORTC	(RCC_APB2ENR |= 1<<4)
+#define RCC_APB2ENR_ENABEL_CLOCK_PORTD	(RCC_APB2ENR |= 1<<5)
+#define RCC_APB2ENR_ENABEL_CLOCK_PORTE	(RCC_APB2ENR |= 1<<6)
+#define RCC_APB2ENR_ENABEL_CLOCK_PORTF	(RCC_APB2ENR |= 1<<7)
+#define RCC_APB2ENR_ENABEL_CLOCK_PORTG	(RCC_APB2ENR |= 1<<8)
+/* GPIO_CRH */
 
+	/*GPIO_CRH_SET_CNF*/
+		/*GPIO_CRH_SET_CNF_INPUT_MODE*/
+#define GPIO_CNF_INPUT_ANALOG		0x00u
+#define GPIO_CNF_INPUT				0x01u
+#define GPIO_CNF_INTPUT_PP			0x02u
+#define GPIO_CNF_RESERVER			0x03u
 
-/* GPIO */
-	/*PORT*/
-#define PORTA				0
-#define PORTB				1
-#define PORTC				2
-#define PORTD				3
-#define PORTE				4
-#define PORTF				5
-#define PORTG				6
-/*GPIO REGISTER*/
-#define BASE_ADDR_GPIO		0x40010800u
+		/*GPIO_CRH_SET_CNF_OUTPUT_MODE*/
+#define GPIO_CNF_OUTPUT_PP			0x00u
+#define GPIO_CNF_OUTPUT_OD			0x01u
+#define GPIO_CNF_ALT_OUTPUT_PP		0x02u
+#define GPIO_CNF_ALT_OUTPUT_OD		0x03u
 
-#define GPIO_MODER(PORT)	(BASE_ADDR_GPIO + PORT*0x400u + 0x00u)
-#define GPIO_IDR(PORT)		(BASE_ADDR_GPIO + PORT*0x400u + 0x08u)
-#define GPIO_MODER_INPUT	0x00u
-#define GPIO_MODER_OUTPUT	0x01u
-#define GPIO_MODER_ALT		0x02u
+	/*GPIO_CRH_SET_MODE*/
+#define GPIO_MODER_INPUT			0x00u
+#define GPIO_MODER_OUTPUT_10Mhz		0x01u
+#define GPIO_MODER_OUTPUT_2Mhz		0x02u
+#define GPIO_MODER_OUTPUT_50Mhz		0x03u
 
-#define GPIOA_PUPDR(PORT)	(BASE_ADDR_GPIO + PORT*0x400u + 0x0Cu)
-#define GPIO_BSRR(PORT)		(BASE_ADDR_GPIO + PORT*0x400u + 0x18u)
+#define GPIO_CRL_MODE(pin)	(4*(pin))
+#define GPIO_CRL_CNF(pin)	((4*(pin))+2)
 
-#define GPIO_CRL(PORT) 		(BASE_ADDR_GPIO + PORT*0x400u + 0x00u) //Port configuration register low (GPIOx_CRL) (2;5;10MHz)
-#define GPIO_CRH(PORT)		(BASE_ADDR_GPIO + PORT*0x400u + 0x04u)
-#define GPIO_ODR(PORT)		(BASE_ADDR_GPIO + PORT*0x400u + 0x0Cu)
-/* Led LD3 - PA5 */
-#define LD3_PIN				15
+#define ADDR_GPIO					0x40010800u
 
-/* user button (B1) - PB11 */
-#define USER_BUTTON			11
+#define GPIO_CRL(port)		*((unsigned int *)(ADDR_GPIO + (port)*0x400 + 0x00u))
+#define GPIO_CRH(port)		*((unsigned int *)(ADDR_GPIO + (port)*0x400 + 0x04u))
+#define GPIO_IDR(port)		*((unsigned int *)(ADDR_GPIO + (port)*0x400 + 0x08u))
+#define GPIO_ODR(port)		*((unsigned int *)(ADDR_GPIO + (port)*0x400 + 0x0Cu))
+#define GPIO_BSRR(port)		*((unsigned int *)(ADDR_GPIO + (port)*0x400 + 0x10u))
 
-/* EXTI */
-#define BASE_ADDR_EXTI		0x40010400u
-#define EXTI_IMR			(BASE_ADDR_EXTI + 0x00u)
-#define EXTI_RTSR			(BASE_ADDR_EXTI + 0x08u)
-#define EXTI_PR				(BASE_ADDR_EXTI + 0x14u)
+#define PORTA				0x00u
+#define PORTB				0x01u
+#define PORTC				0x02u
+#define PORTD				0x03u
+#define PORTE				0x04u
 
-/* AFIO */
-#define BASE_ADDR_AFIO		0x40010000u
-#define AFIO_EXTICR1		(BASE_ADDR_AFIO + 0x08u)
+/*led 1,2,3 PA 4,5,6*/
 
-/* NVIC */
-#define NVIC_ISER			0xE000E100u
-#define NVIC_ICER			0xE000E180u
-#define NVIC_ISPR			0xE000E200u
-#define NVIC_ICPR			0xE000E280u
-#define NVIC_PRI1			0xE000E404u
+#define led_1		4 
+#define led_2		5 
+#define led_3		6 
+
+/*user button PB13,14,15*/
+#define button_1		13 
+#define button_2		14 
+#define button_3		14 
+
+/*EXTI*/
+#define BASE_ADDR_EXTI 				0x40010400u
+#define EXTI_IMR					(BASE_ADDR_EXTI + 0x00u)
+#define EXTI_RTSR					(BASE_ADDR_EXTI + 0x08u)
+#define EXTI_PR						(BASE_ADDR_EXTI + 0x14u)
+
+/*AFIO*/
+#define BASE_ADDR_AFIO				0x40010000u
+#define AFIO_EXTICR4				(BASE_ADDR_AFIO + 0x14u)
+
+/* NVIC*/
+#define NVIC_ISER(num)				(0xE000E100u + (num * 0x04u))
+#define NVIC_ICER(num)				(0XE000E180u + (num * 0x04u))			
+#define	NVIC_ISPR(num)				(0XE000E200u + (num * 0x04u))
+#define NVIC_ICPR(num)				(0XE000E280u + (num * 0x04u))
+#define NVIC_IABR(num)				(0xE000E300u + (num * 0x04u))
+#define NVIC_IPR(num)				(0xE000E100u + (num)) 
 
 /* end file */
 #endif /* __HW_STM32F051R8_H__ */
--- nvic_1/src/main.c
+++ nvic_1/src/main.c
@@ -1,5 +1,4 @@
 #include "hw_stm32f051r8.h"
-void Blink_led();
 /*************************************************************************************************/
 void Reserved_IRQHandler(void)
 {
@@ -49,29 +48,7 @@ void SysTick_Handler(void)
   }
 }
 
-void EXTI0_IRQHandler(void)
-{
-	unsigned int tt;
-	unsigned int temp;
-	/* Ngat cua PA0 */
-	tt = read_reg(GPIO_IDR(PORTB), 1<<15);
-		if(tt)
-		{
-			Blink_led();
-			//delay(200);
-		}
-	/* xoa co ngat */
-	temp = 1 << 0;
-	write_reg(EXTI_PR, temp);
-	write_reg(NVIC_ICPR, (1 << 5));
-}
-void EXTI2_3_IRQHandler(void)
-{
-	while(1)
-	{
-		/* nothing to be run here */
-	}	
-}
+
 /*************************************************************************************************/
 void delay(unsigned int timeout)
 {
@@ -80,34 +57,55 @@ void delay(unsigned int timeout)
     {
         for (t2 = 0; t2 < 0xFFF; t2++)
         {
-          asm(" nop");
+          asm("nop");
         }
     }
 }
 
 void enabled_clock_button()
 {
-	//enable clock portA
-	read_reg(RCC_APB2ENR, ~(1<<2));
-	write_reg(RCC_APB2ENR, (1<<2));
+	//Enabel clock for port A (PA4,5,6 = led1,2,3)
+	RCC_APB2ENR_ENABEL_CLOCK_PORTA;
+
+	//Enabel clock for button PB13,14,15 = button1,2,3
+	RCC_APB2ENR_ENABEL_CLOCK_PORTB;
 }
 
 void Set_Mode_Pin_A()
+
 {
+	/*led PA*/
 	unsigned int i;
-	unsigned int temp_reg;
+	unsigned int tempreg;
 	for(i = 4; i <= 6; i++)
 	{
-		/* xoa du lieu hien tai */
-		read_reg(GPIO_CRL(PORTA), ~(0x3 << 20)); //read_reg(GPIO_CRL(PORTA), ~(0x3 << GPIO_CRL_MODE(i)));
-		/* ghi du lieu moi vao */
-		write_reg(GPIO_CRL(PORTA), (1) << 21);
-		// xoa du lieu hien tai 
-		read_reg(GPIO_CRL(PORTA), ~(0x3 << 22)); //read_reg(GPIO_CRL(PORTA), ~(0x3 << GPIO_CRL_CNF(i)));
-		write_reg(GPIO_CRL(PORTA), 1<< 23); //write_reg(GPIO_CRL(PORTA), (GPIO_CNF_OUTPUT_PP)<< GPIO_CRL_CNF(i));
+		/* set mode CRL */
+		tempreg = read_reg(GPIO_CRL(PORTA), (~(0x3 << GPIO_CRL_MODE(i))));
+		tempreg = write_reg(GPIO_CRL(PORTA), GPIO_MODER_OUTPUT_2Mhz << GPIO_CRL_MODE(i));
+		/*set CRL_CNF*/ /*cho led*/
+		tempreg = read_reg(GPIO_CRL(PORTA), (~(0x3 << GPIO_CRL_CNF(i))));
+		tempreg = write_reg(GPIO_CRL(PORTA),  GPIO_CNF_OUTPUT_PP<< GPIO_CRL_CNF(i));
 	} 	
 }
+void Set_Mode_Pin_B()
 
+{
+	/*button*/
+	unsigned int i;
+	unsigned int j = 3;
+	unsigned int tempreg;
+	for(i = 5; i <= 7; i++)
+	{
+		/* set mode CRH */
+		tempreg = read_reg(GPIO_CRH(PORTB), (~(0x3 << GPIO_CRL_MODE(i))));
+		tempreg = write_reg(GPIO_CRH(PORTB), GPIO_MODER_INPUT << GPIO_CRL_MODE(i));
+		/*set CRL_CNF*/ /*cho button*/
+		tempreg = read_reg(GPIO_CRH(PORTB), (~(0x3 << GPIO_CRL_CNF(i))));
+		tempreg = write_reg(GPIO_CRH(PORTB),  GPIO_MODER_INPUT<< GPIO_CRL_CNF(i));
+		write_reg(GPIO_ODR(PORTB), 	1 << (i*2 + j)); //set pull up page 161 _ RM
+		--j;
+	} 	
+}
 void init_pin()
 {	
 	//set Mode for pin PA4, PA5, PA6
@@ -116,88 +114,131 @@ void init_pin()
 	//Set bit for pin PA4, PA5, PA6
 	//GPIO_BSRR(PORTA) |= 1<<4 | 1<<5 | 1<<6;
 }
-void init_interrupt(void)
+void init_interrupt()
+
 {
 	unsigned int tempreg;
-	/* enable interrupt for EXTI0 */
-	tempreg = read_reg(EXTI_IMR, ~(1 << 0));
-	tempreg = tempreg | (1 << 0);
+	/*enable interrupt for EXTI15*/
+	tempreg = read_reg(EXTI_IMR, ~(1 << 15));
+	tempreg = tempreg | (1 << 15);
 	write_reg(EXTI_IMR, tempreg);
 
-	tempreg = read_reg(EXTI_RTSR, ~(1 << 0));
-	tempreg = tempreg | (1 << 0);
-	write_reg(EXTI_RTSR, tempreg);
-	/* AFIO */
-	tempreg = read_reg(AFIO_EXTICR1, ~(0x0F << 0));
-	tempreg = tempreg | (0x00 << 0);
-	write_reg(AFIO_EXTICR1, tempreg);
-	/* NVIC */
-	/* user button */
-	tempreg = read_reg(NVIC_PRI1, ~(0xFF << 8));
-	tempreg = tempreg | (0x01 << 14);
-	write_reg(NVIC_PRI1, tempreg);
-	
-	/* user button */
-	tempreg = read_reg(NVIC_ISER, ~(1 << 5));
-	tempreg = tempreg | (1 << 5);
-	write_reg(NVIC_ISER, tempreg);
-	/* enable global interrupt */
-	asm("cpsie i");
-	//asm("cpsid i");
+	/*AFIO*/	
+	tempreg = read_reg(AFIO_EXTICR4, ~(0x0F << 15));
+	tempreg = tempreg | (0x01 << 12);
+	write_reg(AFIO_EXTICR4, tempreg);
+
+	/* NVIC*/
+	tempreg = read_reg_8(NVIC_IPR(40), ~(0xFF << 0));
+	tempreg = tempreg | (0x01 << 4);
+	write_reg_8(NVIC_IPR(40), tempreg);
+
+	tempreg = read_reg(NVIC_ISER(40/32), ~(1 << (40%32)));
+	tempreg = tempreg | (1 << 9);
+	write_reg(NVIC_ISER(40/32), tempreg);
 }
-void led_on()
+void led_off(int pin)
 {
 	//Set pin again
-	
-	write_reg(GPIO_BSRR(PORTA), 1<<21); // Turn on led 3 = PA5
-}
-void led_off()
-{
-	//Set pin again
-	write_reg(GPIO_BSRR(PORTA), 1<<5); // Turn off led 3 = PA5
+	write_reg(GPIO_BSRR(PORTA), 1<<(pin));
 }
 
-void Blink_led()
+void led_on(int pin)
+{
+	write_reg(GPIO_BSRR(PORTA), 1<<(pin + 16));
+}
+
+/*void Blink_led()
 {
 	//Reset pin
 	//GPIO_BSRR(PORTA) |= 1<<20 | 1<<21 | 1<<22;
 	unsigned short i, j;
 	for(i = 20, j = 4; i <= 22; ++i)
 	{
-		if(!(read_reg(GPIO_IDR(PORTB), (1<<15))))
+		if(!(GPIO_IDR(PORTB) & (1<<15)))
 		{
 			led_off();
 			break;
 		}
 		//led on
-		write_reg(GPIO_BSRR(PORTA), 1<<i);
+		GPIO_BSRR(PORTA) |= 1<<i;
 		delay(100);
 		//led off
-		write_reg(GPIO_BSRR(PORTA), 1<<(j++));
+		GPIO_BSRR(PORTA) |= 1<<(j++);
 		if(i == 22)
 		{
 			i = 19;
 			j = 4;
 		}
 	}
-}
+}*/
 
-void Init_Button()
-{
-	//clear bit 28-31
-	read_reg(GPIO_CRH(PORTB), (~(0x3 << 28 | 0x3 << 30 ))); //read_reg(GPIO_CRH(PORTB), (~(0x3 << GPIO_CRL_MODE(7) | 0x3 << GPIO_CRL_CNF(7) )));
-	//set 1 bit 31 
-	write_reg(GPIO_CRH(PORTB), 1<<31); //write_reg(GPIO_CRH(PORTB), GPIO_CNF_INTPUT_PP<<GPIO_CRL_CNF(7));
-	write_reg(GPIO_ODR(PORTB), 1<<15);
-}
+
 
 void main(void)
 {
+	unsigned int state_idr_red;
+	unsigned int led_state = 0;
 	enabled_clock_button();
 	init_pin();
-	Init_Button();
+	init_interrupt();
+	//Init_Button();
 	while(1)
 	{
+
+		/*polling*/
+		state_idr_red = read_reg(GPIO_IDR(PORTB), 1 << 13);
+		if ( 0 == state_idr_red) // neu khong chay thi thay 0 = 1 
+		{
+			if (0 == led_state)
+			{
+				led_on(4);
+				delay(50);
+				led_state = 1;
+			}
+			else
+			{
+				led_off(4);
+				delay(50);
+				led_state = 0;
+			}
+		}
+		state_idr_red = read_reg(GPIO_IDR(PORTB), 1 << 14);
+		if ( 0 == state_idr_red)
+		{
+			if (0 == led_state)
+			{
+				led_on(5);
+				delay(50);
+				led_state = 1;
+			}
+			else
+			{
+				led_off(5);
+				delay(50);
+				led_state = 0;
+			}
+		}
+		state_idr_red = read_reg(GPIO_IDR(PORTB), 1 << 15);
+		if ( 0 == state_idr_red)
+		{
+			if (0 == led_state)
+			{
+				led_on(6);
+				delay(50);
+				led_state = 1;
+			}
+			else
+			{
+				led_off(6);
+				delay(50);
+				led_state = 0;
+			}
+		}
 		
+		/*led_off();
+		delay(50);
+		led_on();
+		delay(50);*/
 	}
 }
